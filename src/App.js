7437d3225aa0c11f321618d994d0b71ffeac2421
ShadReza007 <63732556+ShadReza007@users.noreply.github.com>
2024-08-09 20:24:06 +0530

    auto-complete

--- src/App.js
+++ src/App.js
@@ -1,38 +1,89 @@
-import React, { useState } from 'react'
-import axios from 'axios'
+import React, { useState, useEffect, useCallback } from 'react';
+import axios from 'axios';
 
 function App() {
-  const [data,setData] = useState({})
-  const [location,setLocation] = useState('')
+  const [data, setData] = useState({});
+  const [location, setLocation] = useState('');
+  const [suggestions, setSuggestions] = useState([]);
+  
+  const apiKey = '4df984d91c4f6f81447eb1cd17503295';
 
-  const url=`https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&APPID=4df984d91c4f6f81447eb1cd17503295`
+  useEffect(() => {
+    if (location.length > 2) {
+      const fetchSuggestions = async () => {
+        try {
+          const response = await axios.get(`https://api.openweathermap.org/data/2.5/find?q=${location}&type=like&sort=population&cnt=5&appid=${apiKey}`);
+          setSuggestions(response.data.list);
+        } catch (error) {
+          console.error('Error fetching location suggestions:', error);
+        }
+      };
 
-  const searchLocation = (event) => {
-    if(event.key==='Enter'){
-      axios.get(url).then((response) =>{
-        setData(response.data)
-      }).catch((error) => {
-        alert(error.response.data.message)
-      })
-      setLocation('')
+      fetchSuggestions();
+    } else {
+      setSuggestions([]);
     }
-  }
+  }, [location]);
+
+  const searchLocation = (city) => {
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
+    axios.get(url)
+      .then((response) => {
+        setData(response.data);
+        setLocation('');
+        setSuggestions([]);
+      })
+      .catch((error) => {
+        alert(error.response.data.message);
+      });
+  };
+
+  // Debounce function
+  const debounce = (func, delay) => {
+    let timeoutId;
+    return (...args) => {
+      if (timeoutId) clearTimeout(timeoutId);
+      timeoutId = setTimeout(() => {
+        func(...args);
+      }, delay);
+    };
+  };
+
+  const handleKeyPress = (event) => {
+    if (event.key === 'Enter') {
+      debouncedSearchLocation(location);
+    }
+  };
+
+  // Create a debounced version of the searchLocation function
+  const debouncedSearchLocation = useCallback(debounce(searchLocation, 500), []);
 
   return (
     <div className="App">
       <div className='search'>
         <input
-        value={location}
-        onChange={e=>setLocation(e.target.value)}
-        onKeyPress={searchLocation}
-        placeholder='Enter Location'
-        type='text'/>
+          value={location}
+          onChange={e => setLocation(e.target.value)}
+          onKeyPress={handleKeyPress}
+          placeholder='Enter Location'
+          type='text'
+        />
+        {suggestions.length > 0 && (
+          <ul className='autocomplete-list'>
+            {suggestions.map((suggestion) => (
+              <li className='autocomplete-item'
+                key={suggestion.id}
+                onClick={() => searchLocation(suggestion.name)}
+              >
+                {suggestion.name}, {suggestion.sys.country}
+              </li>
+            ))}
+          </ul>
+        )}
       </div>
 
       <div className='container'>
-        
         <div className='top'>
-
           <div className='location'>
             <p>{data.name}</p>
           </div>
@@ -40,33 +91,26 @@ function App() {
             {data.main ? <h1>{data.main.temp.toFixed()}&#176;C</h1> : null}
           </div>
           <div className='description'>
-            {data.weather ? <p className='bold'>{data.weather[0].main}</p> : null }
+            {data.weather ? <p className='bold'>{data.weather[0].main}</p> : null}
           </div>
-
-        </div>
-      
-      {data.name !== undefined && 
-      
-        <div className='bottom'>
-
-          <div className='feels'>
-            {data.main ? <p className='bold'>{data.main.feels_like.toFixed()}&#176;C</p> : null }
-            <p>Feels Like</p>
-          </div>
-          <div className='humidity'>
-            {data.main ? <p className='bold'>{data.main.humidity}%</p> :null}
-            <p>Humidity</p>
-          </div>
-          <div className='wind'>
-            {data.wind ? <p className='bold'>{data.wind.speed.toFixed()}KM/H</p> :null}
-            <p>Wind Speed</p>
-          </div>
-
         </div>
 
-      }
-
-
+        {data.name !== undefined && (
+          <div className='bottom'>
+            <div className='feels'>
+              {data.main ? <p className='bold'>{data.main.feels_like.toFixed()}&#176;C</p> : null}
+              <p>Feels Like</p>
+            </div>
+            <div className='humidity'>
+              {data.main ? <p className='bold'>{data.main.humidity}%</p> : null}
+              <p>Humidity</p>
+            </div>
+            <div className='wind'>
+              {data.wind ? <p className='bold'>{data.wind.speed.toFixed()}KM/H</p> : null}
+              <p>Wind Speed</p>
+            </div>
+          </div>
+        )}
       </div>
     </div>
   );
